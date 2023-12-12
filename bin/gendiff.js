0c9b98a6bef3954ab5cb34c3982bee99e22014c3
roman <123414028+x1nell@users.noreply.github.com>
2023-12-13 00:23:41 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,18 @@
+#!/usr/bin/env node
 
+import { Command } from 'commander';
+import genDiff from '../index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2, program.opts().format);
+    console.log(diff);
+  })
+  .parse();
