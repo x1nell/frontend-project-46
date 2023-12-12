44f1695bb5e8da9358cf583422ff4621a6fd2c3c
roman <123414028+x1nell@users.noreply.github.com>
2023-12-13 00:29:45 +0300

    Update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,1 +1,42 @@
+import _ from 'lodash';
 
+const plain = (tree) => {
+  const iter = (node, acc) => {
+    const setValueText = (value) => {
+      if (_.isObject(value)) {
+        return '[complex value]';
+      }
+
+      if (typeof value === 'string') {
+        return `'${value}'`;
+      }
+
+      return value;
+    };
+
+    const result = node.reduce((accum, obj) => {
+      const name = acc ? `${acc}.${obj.name}` : obj.name;
+      if (obj.status === 'unchanged') {
+        return accum;
+      }
+      if (obj.status === 'added') {
+        return [...accum, `Property '${name}' was added with value: ${setValueText(obj.value)}`];
+      }
+      if (obj.status === 'deleted') {
+        return [...accum, `Property '${name}' was removed`];
+      }
+
+      if (obj.status === 'modified') {
+        return [...accum, `Property '${name}' was updated. From ${setValueText(obj.valueBefore)} to ${setValueText(obj.valueAfter)}`];
+      }
+
+      return [...accum, iter(obj.children, `${name}`)];
+    }, []);
+
+    return result.join('\n');
+  };
+
+  return iter(tree, '');
+};
+
+export default plain;
