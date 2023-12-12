4e71fbcf459ff0b3892e488915da1c900b4754e8
roman <123414028+x1nell@users.noreply.github.com>
2023-12-13 00:27:18 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,1 +1,15 @@
+import jsYaml from 'js-yaml';
 
+const parseFile = (filename, ext) => {
+  if (ext === '.json') {
+    return JSON.parse(filename);
+  }
+
+  if (ext === '.yml' || ext === '.yaml') {
+    return jsYaml.load(filename);
+  }
+
+  return {};
+};
+
+export default parseFile;
