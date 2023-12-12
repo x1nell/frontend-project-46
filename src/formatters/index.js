fb0e00a8bd5fce968a3864605da1483462946771
roman <123414028+x1nell@users.noreply.github.com>
2023-12-13 00:29:16 +0300

    Update index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,1 +1,21 @@
+import stylish from './stylish.js';
+import plain from './plain.js';
+import json from './json.js';
 
+const formatTree = (tree, format) => {
+  if (format === 'stylish') {
+    return stylish(tree);
+  }
+
+  if (format === 'plain') {
+    return plain(tree);
+  }
+
+  if (format === 'json') {
+    return json(tree);
+  }
+
+  return {};
+};
+
+export default formatTree;
